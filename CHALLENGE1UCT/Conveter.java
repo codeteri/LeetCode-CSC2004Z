7232c4ad44c5367821570d3dce3a049d3ebbddce
WAVYTERRY <codeeeterry@gmail.com>
2024-04-19 14:20:07 +0200

    created file to convert time to keywords

---
+++ CHALLENGE1UCT/Conveter.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+String[] hours = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve"};
+String[] minutes = {"five" , "ten", "quarter", "twenty", "half"};
+
+class Main {
+  public static void main(String[] args){
+  Scanner Obj = new Scanner(System.in);
+
+  System.out.println("Enter a time or 'quit':");
+  String time = Obj.nextLine();
+  
+
+  
+  }
+}
